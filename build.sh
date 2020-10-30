b0bb61bbe6a33d6867e8d9caf0161c5bafda7b6b
Elliot Rodgers <elliot.rodgers@sykescottages.co.uk>
2020-10-30 15:07:11 +0000

    Test phpcs fails

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-vendor/bin/phpcs --standard=Doctrine src
-vendor/bin/phpcs --standard=Doctrine tests
+
+vendor/bin/phpcs --standard=Doctrine --report-json=./phpcs.json src
+
+errors=($(jq -r '.totals.errors' phpcs.json))
+
+if (( $errors > 0 )); then
+  exit 1
+fi
+
 vendor/bin/phpunit
